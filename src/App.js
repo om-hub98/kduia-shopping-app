863087a1c5bcbe330a17be84be0130ee5d1967e0
Omraj Pradhan <omrajpradhan@gmail.com>
2024-01-22 00:14:06 +0530

    [UPD] app component, expense list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import { AppProvider } from './context/AppContext';
 import CartValue from './components/CartValue';
@@ -7,8 +7,50 @@ import ItemSelected from './components/ItemSelected';
 import Location from './components/Location';
 
 function App() {
+
+  const [ budget, setBudget ] = useState(2000);
+  const [ spendSoFar, setSpentSoFar ] = useState(960);
+
+  const handleBudgetChange=(event)=>{
+    const budget=event.target.value;
+    if(budget <= 20000 && budget >= spendSoFar){
+      setBudget(budget);
+    }
+    
+  }
+
   return (
-    <div className="App">
+    <div className="App" style={{disply:"flex", width:"100%",justifyContent:"center"}}>
+      <h1>Company's Budget Allocation </h1>
+      <div className="container text-center">
+        <div className="row">
+            <div className="col">
+              Budget: £ <input type="number" value={budget} onChange={handleBudgetChange} />
+            </div>
+            <div className="col">
+              Remaining: £{budget-spendSoFar}
+            </div>
+            <div className="col">
+              Spent so far: £{spendSoFar}
+            </div>
+            <div className="col">
+              <div className="dropdown-center">
+                <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Centered dropdown
+                </button>
+                    <ul className="dropdown-menu">
+                      <li><a className="dropdown-item" href="#">Action</a></li>
+                      <li><a className="dropdown-item" href="#">Action two</a></li>
+                      <li><a className="dropdown-item" href="#">Action three</a></li>
+                    </ul>
+              </div>
+            </div>
+        </div>
+      </div>
+      <div style={{justifyContent:"center", width:"80%"}}>
+        <h3>Allocation</h3>
+        <ExpenseList />
+      </div>   
     </div>
   );
 }
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -2,6 +2,39 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseList = () => {
+    return(
+        <div >
+            <table class="table">
+                <thead>
+                    <tr>
+                    <th scope="col">Department</th>
+                    <th scope="col">Allocated Budget</th>
+                    <th scope="col">Increase by 10</th>
+                    <th scope="col">Decrease by 10</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">3</th>
+                    <td colspan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
 };
 
 export default ExpenseList;

3045310eabb7daf0fe8b1ad7e2b95101efc0b7d5
Omraj Pradhan <omrajpradhan@gmail.com>
2024-01-29 23:12:04 +0530

    [ADD] design for shopping cart

--- src/App.js
+++ src/App.js
@@ -1,56 +1,193 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-import { AppProvider } from './context/AppContext';
-import CartValue from './components/CartValue';
-import ExpenseList from './components/ExpenseList';
-import ItemSelected from './components/ItemSelected';
-import Location from './components/Location';
+/** @format */
+
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { AppProvider } from "./context/AppContext";
+import CartValue from "./components/CartValue";
+import ExpenseList from "./components/ExpenseList";
+import ItemSelected from "./components/ItemSelected";
+import Location from "./components/Location";
+import { DropdownButton } from "react-bootstrap";
+import Dropdown from "react-bootstrap/Dropdown";
+import "./App.css";
 
 function App() {
+  const [budget, setBudget] = useState(2000);
+  const [spentSoFar, setSpentSoFar] = useState(960);
+  const [currency, setCurrency] = useState("£ Pound");
+  const [currencySymbol, setCurrencySymbol] = useState("£");
+  const [remainingFund, setRemainingFund] = useState(0);
 
-  const [ budget, setBudget ] = useState(2000);
-  const [ spendSoFar, setSpentSoFar ] = useState(960);
-
-  const handleBudgetChange=(event)=>{
-    const budget=event.target.value;
-    if(budget <= 20000 && budget >= spendSoFar){
+  const handleBudgetChange = (event) => {
+    const budget = event.target.value;
+    if(budget < spentSoFar){
+      alert("Budget cannot be less than spent so far !!!!")
+    }
+    if(budget > 20000){
+      alert("Budget cannot be exceed £ 20000 !!!!")
+    }
+    if (budget <= 20000 && budget >= spentSoFar) {
       setBudget(budget);
     }
-    
-  }
-
+  };
+  const handleDropDown = (eventKey,event) => {
+    console.log(event.target.text);
+    setCurrency(event.target.text);
+    setCurrencySymbol(eventKey)
+  };
   return (
-    <div className="App" style={{disply:"flex", width:"100%",justifyContent:"center"}}>
+    <div className="App" style={{ disply: "flex", justifyContent: "center" }}>
       <h1>Company's Budget Allocation </h1>
-      <div className="container text-center">
-        <div className="row">
-            <div className="col">
-              Budget: £ <input type="number" value={budget} onChange={handleBudgetChange} />
-            </div>
-            <div className="col">
-              Remaining: £{budget-spendSoFar}
-            </div>
-            <div className="col">
-              Spent so far: £{spendSoFar}
-            </div>
-            <div className="col">
-              <div className="dropdown-center">
-                <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Centered dropdown
-                </button>
-                    <ul className="dropdown-menu">
-                      <li><a className="dropdown-item" href="#">Action</a></li>
-                      <li><a className="dropdown-item" href="#">Action two</a></li>
-                      <li><a className="dropdown-item" href="#">Action three</a></li>
-                    </ul>
-              </div>
-            </div>
+      <div
+        className="container text-center"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <div
+          className="col"
+          style={{
+            padding: "0.6rem 0.3rem 0.6rem 1.8rem",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            marginRight: "20px",
+            backgroundColor: "lightgray",
+            borderRadius: "0.3rem",
+          }}
+        >
+          <span
+            style={{ display: "flex", flexDirection: "row", width: "5rem" }}
+          >
+            Budget: {currencySymbol}
+          </span>
+          <input
+            type="number"
+            value={budget}
+            onChange={handleBudgetChange}
+            step="10"
+          />
+        </div>
+        <div
+          className="col"
+          style={{
+            padding: "0.7rem",
+            marginRight: "1rem",
+            borderRadius: "0.3rem",
+            backgroundColor: "lightgreen",
+            color: "darkgreen",
+            display: "flex",
+            justifyContent: "flex-start",
+          }}
+        >
+          Remaining: {currencySymbol}{(budget - spentSoFar) <= 0 ? alert("Spending cannot exceed the remaining value") : budget - spentSoFar}
+        </div>
+        <div
+          className="col"
+          style={{
+            padding: "0.7rem",
+            marginRight: "1rem",
+            borderRadius: "0.3rem",
+            backgroundColor: "lightblue",
+            color: "blue",
+            display: "flex",
+            justifyContent: "flex-start",
+          }}
+        >
+          Spent so far: {currencySymbol}{spentSoFar}
+        </div>
+        <div className="col">
+          <DropdownButton
+            id="dropdown-button-dark-example2"
+            variant="secondary"
+            title={`Currency(${currency})`}
+            className="currency"
+            style={{
+              backgroundColor: "lightgreen",
+              border: "none",
+              padding: "0.07rem",
+              borderRadius: "0.3rem",
+            }}
+            onSelect={handleDropDown}
+          >
+            <Dropdown.Item eventKey="$">$ Dollar</Dropdown.Item>
+            <Dropdown.Item eventKey="£">£ Pound</Dropdown.Item>
+            <Dropdown.Item eventKey="€">€ Euro</Dropdown.Item>
+            <Dropdown.Item eventKey="₹">₹ Ruppee</Dropdown.Item>
+          </DropdownButton>
         </div>
       </div>
-      <div style={{justifyContent:"center", width:"80%"}}>
-        <h3>Allocation</h3>
-        <ExpenseList />
-      </div>   
+      <div style={{ justifyContent: "center", width: "80%" }}>
+        <h3 style={{ margin: "3rem 3rem 1rem 3rem" }}>Allocation</h3>
+        <ExpenseList 
+          setSpentSoFar={setSpentSoFar} 
+          spentSoFar={spentSoFar}
+          currencySymbol={currencySymbol}
+         />
+      </div>
+      <div
+        style={{ marginLeft: "3rem", marginBottom: "1rem", marginTop: "2rem" }}
+      >
+        <h3>Change Allocation</h3>
+        <div
+          style={{ display: "flex", justifyContent: "left", marginTop: "1rem" }}
+        >
+          <div
+            style={{
+              marginRight: "2rem",
+              backgroundColor: "#E3E3E3",
+              paddingLeft: "1rem",
+              borderTopLeftRadius: "0.3rem",
+              borderBottomLeftRadius: "0.3rem",
+            }}
+          >
+            <label for="budget" style={{ marginRight: "1rem" }}>
+              Department{" "}
+            </label>
+            <select name="budget" id="budget">
+              <option value="choose">Choose...</option>
+              <option value="IT">IT</option>
+              <option value="HR">Human Resource</option>
+              <option value="sales">Sales</option>
+            </select>
+          </div>
+          <div
+            style={{
+              marginRight: "2rem",
+              backgroundColor: "#E3E3E3",
+              paddingLeft: "1rem",
+              borderTopLeftRadius: "0.3rem",
+              borderBottomLeftRadius: "0.3rem",
+            }}
+          >
+            <label for="allocation" style={{ marginRight: "1rem" }}>
+              Allocation{" "}
+            </label>
+            <select name="allocation" id="allocation">
+              <option value="add">Add</option>
+              <option value="subtract">Subtract</option>
+            </select>
+          </div>
+          <div style={{ marginRight: "2rem" }}>
+            {currencySymbol} <input />
+          </div>
+          <div style={{ marginRight: "2rem" }}>
+            <button
+              style={{
+                backgroundColor: "#318CE7",
+                border: "none",
+                borderTopRightRadius: "0.3rem",
+                borderBottomRightRadius: "0.3rem",
+              }}
+            >
+              Save
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,40 +1,183 @@
-import React, { useContext } from 'react';
-import { AppContext } from '../context/AppContext';
+/** @format */
 
-const ExpenseList = () => {
-    return(
-        <div >
-            <table class="table">
-                <thead>
-                    <tr>
-                    <th scope="col">Department</th>
-                    <th scope="col">Allocated Budget</th>
-                    <th scope="col">Increase by 10</th>
-                    <th scope="col">Decrease by 10</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    )
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+
+const ExpenseList = ({spentSoFar, setSpentSoFar, currencySymbol}) => {
+
+    const handlePlusIcon = () => {
+        setSpentSoFar(spentSoFar+10);
+    }
+    const handleMinusIcon = () => {
+        setSpentSoFar(spentSoFar-10);
+    }
+    
+  return (
+    <div>
+      <table class="table">
+        <thead style={{textAlign:"center"}}>
+          <tr>
+            <th scope="col">Department</th>
+            <th scope="col">Allocated Budget</th>
+            <th scope="col">Increase by 10</th>
+            <th scope="col">Decrease by 10</th>
+          </tr>
+        </thead>
+        <tbody style={{textAlign:"center"}}>
+          <tr>
+            <th style={{fontWeight:"normal"}}>Marketing</th>
+            <td>{currencySymbol} 50</td>
+            <td>
+              <svg  
+                xmlns="http://www.w3.org/2000/svg"
+                width="25"
+                height="25"
+                fill="#6AC36A"
+                class="bi bi-plus-circle-fill"
+                viewBox="0 0 16 16"
+                onClick={handlePlusIcon}
+              >
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
+              </svg>
+            </td>
+            <td>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="25"
+                height="25"
+                fill="#C30D30"
+                class="bi bi-dash-circle-fill"
+                viewBox="0 0 16 16"
+                onClick={handleMinusIcon}
+              >
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1z" />
+              </svg>
+            </td>
+          </tr>
+          <tr>
+            <th style={{fontWeight:"normal"}}>Finance</th>
+            <td>{currencySymbol} 300</td>
+            <td>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="25"
+                height="25"
+                fill="#6AC36A"
+                class="bi bi-plus-circle-fill"
+                viewBox="0 0 16 16"
+                onClick={handlePlusIcon}
+              >
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
+              </svg>
+            </td>
+            <td>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="25"
+                height="25"
+                fill="#C30D30"
+                class="bi bi-dash-circle-fill"
+                viewBox="0 0 16 16"
+                onClick={handleMinusIcon}
+              >
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1z" />
+              </svg>
+            </td>
+          </tr>
+          <tr>
+            <th style={{fontWeight:"normal"}}>Sales</th>
+            <td>{currencySymbol} 70</td>
+            <td>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="25"
+                height="25"
+                fill="#6AC36A"
+                class="bi bi-plus-circle-fill"
+                viewBox="0 0 16 16"
+                onClick={handlePlusIcon}
+              >
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
+              </svg>
+            </td>
+            <td>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="25"
+                height="25"
+                fill="#C30D30"
+                class="bi bi-dash-circle-fill"
+                viewBox="0 0 16 16"
+                onClick={handleMinusIcon}
+              >
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1z" />
+              </svg>
+            </td>
+          </tr>
+          <tr>
+            <th style={{fontWeight:"normal"}}>Human Resource</th>
+            <td>{currencySymbol} 40</td>
+            <td>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="25"
+                height="25"
+                fill="#6AC36A"
+                class="bi bi-plus-circle-fill"
+                viewBox="0 0 16 16"
+                onClick={handlePlusIcon}
+              >
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
+              </svg>
+            </td>
+            <td>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="25"
+                height="25"
+                fill="#C30D30"
+                class="bi bi-dash-circle-fill"
+                viewBox="0 0 16 16"
+                onClick={handleMinusIcon}
+              >
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1z" />
+              </svg>
+            </td>
+          </tr>
+          <tr>
+            <th style={{fontWeight:"normal"}}>IT</th>
+            <td>{currencySymbol} 500</td>
+            <td>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="25"
+                height="25"
+                fill="#6AC36A"
+                class="bi bi-plus-circle-fill"
+                viewBox="0 0 16 16"
+                onClick={handlePlusIcon}
+              >
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
+              </svg>
+            </td>
+            <td>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="25"
+                height="25"
+                fill="#C30D30"
+                class="bi bi-dash-circle-fill"
+                viewBox="0 0 16 16"
+                onClick={handleMinusIcon}
+              >
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1z" />
+              </svg>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default ExpenseList;
